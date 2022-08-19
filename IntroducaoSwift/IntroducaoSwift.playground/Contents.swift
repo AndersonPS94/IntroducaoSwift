1d7c56abf3d947bd454a426f8c2eebc6d2152fbc
AndersonPS94 <dson_adn@hotmail.com>
2022-08-19 01:09:07 -0300

    commit 3

--- IntroducaoSwift/IntroducaoSwift.playground/Contents.swift
+++ IntroducaoSwift/IntroducaoSwift.playground/Contents.swift
@@ -35,3 +35,83 @@ occupations["Jayne"] = "Public Relations"
 
 let emptyArray: [String] = []
 let emptyDicionary: [String: Float] = [:]
+// End Simple Values
+
+// Begin Control Flow
+let individualScore = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScore {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore)
+
+var optinalString: String? = "Hello"
+print(optinalString == nil)
+
+var optinalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optinalName {
+    greeting = "Hello, \(name)"
+}
+
+let nickname: String? = nil
+let fullName: String = "John Apllessed"
+let informalGreeting = "Hi \(nickname ?? fullName)"
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a long.")
+case "cucumber", "watercress":
+    print("That wold make a good tea sandwich.")
+case let x where x.hasSuffix("pepeer"):
+    print("Is a spicy \(x)?")
+default:
+    print("Everyting tastes good in soup.")
+}
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 11, 13],
+    "Fibtonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25]
+]
+var largest = 0
+var key = ""
+for (_key, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+            key = _key
+        }
+    }
+}
+print(largest)
+print(key)
+
+var n = 2
+while n < 100 {
+    n *= 2
+}
+print(n)
+
+var m = 2
+repeat {
+    m *= 2
+} while m < 100
+print(m)
+
+var total = 0
+for i in 0..<4 {
+    total += i
+}
+print(total)
+
+var newTotal = 0
+for i in 0...4 {
+    newTotal += i
+}
+print(newTotal)
